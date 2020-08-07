6b8cbcb2db5db9d0d20411f9108c4908ba77a5f5
name <name@localhost.local>
2020-08-07 14:47:20 +1000

    fng001 inital commit for fake news generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+// import { useState } from 'react';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
+import FeedReader from './feed-reader';
+import { auFeeds } from './feeds/au_rss_news_feeds.json';
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>FAKE NEWS</h1>
+        </header>
+        <main>
+          <FeedReader feeds={auFeeds}></FeedReader>
+        </main>
+      </div>
   );
 }
 
