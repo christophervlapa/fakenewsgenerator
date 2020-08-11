7e29bb6e8b67ae26824d30dc93d4efd966b0d4c9
name <name@localhost.local>
2020-08-11 19:28:28 +1000

    fn001 Moved project to AngularJS bc React was getting messy

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
+const http = require('http');
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end('Hello, World!');
+  }
+  
+  const server = http.createServer(requestListener);
+  server.listen(8080);
 
 class MakeFeedFile {
 
@@ -11,14 +20,14 @@ class MakeFeedFile {
         this.todaysJsonFeedTitlesFile = () => {
             const today = new Date();
             const todaysDate = ("0" + today.getDate()).slice(-2) + '-' + ("0" + (today.getMonth()+1)).slice(-2) + '-' + ("0" + today.getFullYear()).slice(-2);
-            return `./public/feed-title-files/feed-titles_${todaysDate}.csv`;
+            return `./public/feed-title-files/feed-titles_${todaysDate}.json`;
         }
 
         this.convertTitlesArrayToCSV = ( titlesArray ) => {
             return titlesArray.join("/n");
         }
 
-        this.writeTodaysFeedFile = async () => {
+        this.writeTodaysFeedFile = async (readFile) => {
 
             let newTitlesArray = [];
             let rawFeedsJSON = this.fs.readFileSync('./public/au_rss_news_feeds.json');
@@ -29,50 +38,57 @@ class MakeFeedFile {
                     return (async () => {
                         let feed = await parser.parseURL(feedURL);
                         feed.items.forEach(item => {
-                            newTitlesArray.push(item.title);
+                            newTitlesArray.push('"' + item.title + '"');
                             return item.title;
                         });
                     })()
                 })
             ).then(() => {
                 console.log("newTitlesArray ",newTitlesArray);
-                this.fs.writeFile(this.todaysJsonFeedTitlesFile(), newTitlesArray.join("\n"), function (err) {
+                // create the JSON string to write to today's title file:
+                const todaysTempJSON = `{"titles" :[
+                        ${newTitlesArray.join(",")}
+                ]}
+                    `;
+                    console.log("JSON ",todaysTempJSON);
+
+                this.fs.writeFile(this.todaysJsonFeedTitlesFile(), todaysTempJSON, function (err) {
                     if (err) return console.log(err);
-                    console.log(newTitlesArray.length + ' written to new file ' + this.todaysJsonFeedTitlesFile());
+                    console.log(newTitlesArray.length + ' written to new file ');
                 });
             }).catch(reasons => {
-                console.log("results ",reasons);
+                console.log("Error somewhere with RSS feed promises: ",reasons);
             });
 
             
         }
 
-        this.readTodaysFeedTitles = () => {
+        // this.readTodaysFeedTitles = () => {
 
-            let tempWordsArray = [];
+        //     let tempWordsArray = [];
 
-            const titles = new Promise((resolve) => {
+        //     const titles = new Promise((resolve) => {
 
-                this.fs.readFile(this.todaysJsonFeedTitlesFile(), 'utf8', function (err,data) {
-                    if(err){ throw err; }
-                    let titlesArray = data.split("\n");
-                    titlesArray.forEach((titleLine) => {
-                        titleLine = titleLine.replace(/[^a-zA-Z0-9 ]/g, "").toLocaleLowerCase();
-                        tempWordsArray.push(...titleLine.split(" "));
-                    })
+        //         this.fs.readFile(this.todaysJsonFeedTitlesFile(), 'utf8', function (err,data) {
+        //             if(err){ throw err; }
+        //             let titlesArray = data.split("\n");
+        //             titlesArray.forEach((titleLine) => {
+        //                 titleLine = titleLine.replace(/[^a-zA-Z0-9 ]/g, "").toLocaleLowerCase();
+        //                 tempWordsArray.push(...titleLine.split(" "));
+        //             })
     
-                    let wordsArray =  [...new Set(tempWordsArray)];
+        //             let wordsArray =  [...new Set(tempWordsArray)];
     
-                    resolve(wordsArray);
+        //             resolve(wordsArray);
     
-                });
-            });
+        //         });
+        //     });
     
-            titles.then((titlesArray) => {
-                console.log("titlesArray " + titlesArray);
-                this.wordsArray = titlesArray;   
-            })
-        }
+        //     titles.then((titlesArray) => {
+        //         console.log("titlesArray " + titlesArray);
+        //         this.wordsArray = titlesArray;   
+        //     })
+        // }
     }
     
 
@@ -81,16 +97,13 @@ class MakeFeedFile {
         const todaysFile = this.todaysJsonFeedTitlesFile();
 
         try {
-            if (this.fs.existsSync(todaysFile)) {
-                console.log("todaysFile YES");
-                this.readTodaysFeedTitles();
-            } else {
+            if (!this.fs.existsSync(todaysFile)) {
                 console.log("todaysFile NO");
                 this.writeTodaysFeedFile();
-                this.readTodaysFeedTitles();
+                // this.readTodaysFeedTitles();
             }
         } catch(err) {
-            
+            console.log("Uh oh! An error: ",err);
         }        
     }
 }
